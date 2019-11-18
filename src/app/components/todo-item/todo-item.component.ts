5f05c4b382605250cb44fcc3347e3335b2403748
mario <duythientran1998@gmail.com>
2019-11-18 22:26:55 +0700

    update layout

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-item.component.scss']
 })
 export class TodoItemComponent implements OnInit {
-
+  isEditing = false;
   constructor() { }
 
   ngOnInit() {
